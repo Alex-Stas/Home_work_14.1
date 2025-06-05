dcdd752930c15dfddb0ffff84746f3a1865eaa60
Alex-Stas <alexander.stashchuk@gmail.com>
2025-06-05 15:16:31 +0300

    New product classed added with add modification to check class type

--- src/product.py
+++ src/product.py
@@ -14,7 +14,9 @@ class Product:
         return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other):
-        return self.__price * self.quantity + other.__price * other.quantity
+        if type(other) is Product:
+            return self.__price * self.quantity + other.__price * other.quantity
+        raise TypeError
 
     @classmethod
     def new_product(cls, dict_with_arg):
@@ -33,25 +35,26 @@ class Product:
 
 
 if __name__ == "__main__":
-    pass
-    # product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    #
-    # print(product.name)
-    # print(product.description)
-    # print(product.price)
-    # print(product.quantity)
-    #
-    # product_dict = {"name": "Nokia 7730", "description": "Best phone ever", "price": 10000, "quantity": 5}
-    #
-    # product2 = Product.new_product(product_dict)
-    #
-    # print(product2.name)
-    # print(product2.description)
-    # print(product2.price)
-    # print(product2.quantity)
-    #
-    # product2.price = -25
-    # product2.price = 9800
-    # print(product2.price)
-    #
-    # print(product + product2)
+    # pass
+    product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+
+    print(product.name)
+    print(product.description)
+    print(product.price)
+    print(product.quantity)
+
+    product_dict = {"name": "Nokia 7730", "description": "Best phone ever", "price": 10000, "quantity": 5}
+
+    product2 = Product.new_product(product_dict)
+
+    print(product2.name)
+    print(product2.description)
+    print(product2.price)
+    print(product2.quantity)
+
+    product2.price = -25
+    product2.price = 9800
+    print(product2.price)
+
+    print(product + product2)
+    print(product + 5)
