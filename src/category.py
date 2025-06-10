be294365c8f48e1fd4814c22afb1ea7d235ae2ad
Alex-Stas <alexander.stashchuk@gmail.com>
2025-06-10 19:35:58 +0300

    All functions added, price method renamed

--- src/category.py
+++ src/category.py
@@ -1,6 +1,6 @@
 from src.product import Product
 
-# from src.smartphone import Smartphone
+from src.smartphone import Smartphone
 
 
 class Category:
@@ -41,39 +41,58 @@ class Category:
     def products_in_list(self):
         return self.__products
 
+    def middle_price(self):
+        try:
+            return sum([product.price for product in self.products_in_list]) / len(self.products_in_list)
+        except ZeroDivisionError:
+            return 0
+
 
 if __name__ == "__main__":
-    pass
-    # product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    # product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    # product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-    #
-    # category = Category(
-    #     "Смартфоны",
-    #     "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-    #     [product1, product2, product3],
-    # )
-    #
-    # print(category.name)
-    # print(category.description)
-    # print(category.products)
-    #
-    # product4 = Product("Nokia 3310", "Just a phone", 5000, 20)
-    #
-    # category.add_product(product4)
-    #
-    # print(category.products)
-    # print(category.category_count)
-    # print(category.product_count)
-    #
-    # print(category)
-    #
-    # product5 = Smartphone("Nokia 3310", "Just a phone", 5000, 50, 90.3, "Note 11", 1024, "Синий")
-    #
-    # category.add_product("product5")
-    #
-    # print(category.products)
-    # print(category.category_count)
-    # print(category.product_count)
-    #
-    # print(category)
+    # pass
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+
+    print(category.name)
+    print(category.description)
+    print(category.products)
+
+    product4 = Product("Nokia 3310", "Just a phone", 5000, 20)
+
+    category.add_product(product4)
+
+    print(category.products)
+    print(category.category_count)
+    print(category.product_count)
+
+    print(category)
+
+    product5 = Smartphone("Nokia 3310", "Just a phone", 5000, 50, 90.3, "Note 11", 1024, "Синий")
+
+    category.add_product(product5)
+
+    print(category.products)
+    print(category.category_count)
+    print(category.product_count)
+
+    print(category)
+
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 10000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 20000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 15000.0, 14)
+
+    category3 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни")
+
+    print()
+    print(category3.average_price_product())
+
+
